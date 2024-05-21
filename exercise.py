5cecc5c0da8697bc2dbdad5f23895dc1fe8fbd99
venibee <svenja@hemmers-online.de>
2024-05-21 18:41:02 +0200

    lengthlastword

--- exercise.py
+++ exercise.py
@@ -1,5 +1,10 @@
-import math
-import os
-print("print")
-print("one more sheng")
-print("one mooouuua sheng")
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        x = s.rstrip(' ')
+        if ' ' in x:
+            start=x.rfind(' ')
+            lastword=x[start:]
+            k = len(lastword)-1
+        else:
+            k=len(x)
+        return k
